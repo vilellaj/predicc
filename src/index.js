01bbccc9bf01c9429d989f100c32a8c883825a68
Jonathan Vilella <vilellaj@hotmail.com>
2019-05-31 10:50:31 -0300

    Ajuste

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const rawData = [];
 const dataFilePath = 'data/data.csv';
 
 let target = process.argv[2];
+let targetNumber = process.argv[3];
 let targetSplit = target.split('/');
 let targetDate = new Date(targetSplit[2], targetSplit[1], targetSplit[0]);
 
@@ -23,13 +24,8 @@ const parseFile = async (path) => {
         .on('data', (item) => {
             rawData.push({
                 date: parseDate(item.date),
-                dez1: parseInt(item.dez1),
-                dez2: parseInt(item.dez2),
-                dez3: parseInt(item.dez3),
-                dez4: parseInt(item.dez4),
-                dez5: parseInt(item.dez5),
-                dez6: parseInt(item.dez6),
-            })
+                value: parseInt(item[`dez${targetNumber}`])
+            });
         })
         .on('end', async () => {
             await predict();
@@ -40,36 +36,36 @@ const predict = async () => {
 
     // Build and compile model.
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 6, inputShape: [1] }));
+    model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 
     const learningRate = 0.05
     const sgdOptimizer = tf.train.sgd(learningRate)
-
     model.compile({ loss: 'meanSquaredError', optimizer: sgdOptimizer });
 
     let min = rawData[rawData.length - 1].date.getTime();
     let max = targetDate.getTime();
 
     const rawDataXs = rawData.map((item) => {
-        return normalize(item.date.getTime(), min, max);
+        return [normalize(item.date.getTime(), min, max)];
     });
 
     const rawDataYs = rawData.map((item) => {
-        return [item.dez1, item.dez2, item.dez3, item.dez4, item.dez5, item.dez6]
+        return [item.value]
     });
 
     //Inputs
     const xs = tf.tensor2d(rawDataXs, [rawDataXs.length, 1]);
 
     // Outputs
-    const ys = tf.tensor2d(rawDataYs, [rawDataYs.length, 6]);
+    const ys = tf.tensor2d(rawDataYs, [rawDataYs.length, 1]);
 
     // Train model with fit().
     await model.fit(xs, ys, { epochs: 300 });
 
     // Run inference with predict().
-    let prediction = model.predict(tf.tensor2d([normalize(targetDate.getTime(), min, max)], [1, 1]));
-    const readable = prediction.dataSync();
+    let targetValue = normalize(targetDate.getTime(), min, max);
+    let prediction = model.predict(tf.tensor2d([targetValue], [1, 1]));
+    //const readable = prediction.dataSync();
     prediction.print();
 }
 
